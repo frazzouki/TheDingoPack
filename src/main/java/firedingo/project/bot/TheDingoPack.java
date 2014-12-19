227d959e37744182d121df264b277dde1c260d69
firedingo <firedingo@users.noreply.github.com>
2014-12-19 21:54:34 +1100

    Updating Changes Added. Bot Should Be Basically Setup but do nothing so far. :)

--- src/main/java/firedingo/project/bot/TheDingoPack.java
+++ src/main/java/firedingo/project/bot/TheDingoPack.java
@@ -8,9 +8,9 @@ public class TheDingoPack {
 	
 	Configuration config = new Configuration.Builder()
     .setName(firedingo.project.bot.reference.Reference.NICK) //Nick of the bot. CHANGE IN YOUR CODE
-    .setLogin(" ") //Login part of hostmask, eg name:login@host
+    //.setLogin(" ") //Login part of hostmask, eg name:login@host (MIGHT NOT BE NEEDED)
     .setAutoNickChange(true) //Automatically change nick when the current one is in use
-    .setServer(" ") //The server were connecting to
+    .setServer(firedingo.project.bot.reference.Reference.HOST, firedingo.project.bot.reference.Reference.PORT) //The server were connecting to
     .addAutoJoinChannel("#TheDingoPack") //Join #TheDingoPack channel on connect
     .buildConfiguration(); //Create an immutable configuration from this builder
 
--- src/main/java/firedingo/project/bot/reference/Reference.java
+++ src/main/java/firedingo/project/bot/reference/Reference.java
@@ -3,4 +3,8 @@ package firedingo.project.bot.reference;
 public class Reference {
 
 	public static final String NICK = "TheDingoPack";
+	public static final String HOST = "irc.twitch.com";
+	public static final String OAUTH = "";
+	public static final int PORT = 6667;
+	public static final String PASS = "";
 }
