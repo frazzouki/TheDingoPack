b955df3febbd4bbd45cd05dd5593874143cb681a
firedingo <firedingo@users.noreply.github.com>
2014-12-19 22:19:50 +1100

    Fixes a slight derp in the irc address on my part. :P

--- src/main/java/firedingo/project/bot/reference/Reference.java
+++ src/main/java/firedingo/project/bot/reference/Reference.java
@@ -3,8 +3,8 @@ package firedingo.project.bot.reference;
 public class Reference {
 
 	public static final String NICK = "TheDingoPack";
-	public static final String HOST = "irc.twitch.com";
+	public static final String HOST = "irc.twitch.tv";
 	public static final String OAUTH = "";
 	public static final int PORT = 6667;
-	public static final String PASS = "";
+
 }
