63cb5c75cb9dfd2453a95ea53023dc99584046a2
firedingo <firedingo@users.noreply.github.com>
2014-12-19 22:35:35 +1100

    Just a small update with some line commenting added

--- src/main/java/firedingo/project/bot/reference/Reference.java
+++ src/main/java/firedingo/project/bot/reference/Reference.java
@@ -2,6 +2,7 @@ package firedingo.project.bot.reference;
 
 public class Reference {
 
+	//Connection Details As Constants For Easy Customizability
 	public static final String NICK = "TheDingoPack";
 	public static final String HOST = "irc.twitch.tv";
 	public static final String OAUTH = "";
