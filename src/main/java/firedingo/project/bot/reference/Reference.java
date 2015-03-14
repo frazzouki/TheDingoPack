fc73af1932f1edf89c73d264355d0c0c1a23b79a
firedingo <firedingo@users.noreply.github.com>
2015-03-14 22:00:17 +1100

    Makes Some Changes in prep for a buggy Prerelease.

--- src/main/java/firedingo/project/bot/reference/Reference.java
+++ src/main/java/firedingo/project/bot/reference/Reference.java
@@ -10,13 +10,13 @@ public class Reference {
     //Connection Details As Constants For Easy Customizability
     public static final String NICK = "thedingopack";
     public static final String HOST = "irc.twitch.tv";
-    public static final String OAUTH = "oauth:phc2e9y628cr1cyv2e3ktmugfdwsvc1";
+    public static final String OAUTH = "oauth:";
     public static final int PORT = 6667;
     //these two channels can be changed as necessary, extras can also be added.
     public static final String BOTCHAN = "#thedingopack";
     public static final String MODCHAN = "#firedingo99365";
 
     //Bot Specific fields
-    public static final String VERSION = "1.0";
+    public static final String VERSION = "0.3";
     public static boolean visitor = false;
 }
